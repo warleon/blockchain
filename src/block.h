a90e6ae81e37dcac041046b60f0bb5fcef26c93c
warleon <51892390+warleon@users.noreply.github.com>
2023-05-06 13:22:39 -0500

    Update block.h

--- src/block.h
+++ src/block.h
@@ -1,8 +1,32 @@
+#include <stdio.h>
+
 #include "common_types.h"
 
 typedef size_t nonce_t;
+typedef size_t hash_t;
+typedef unsigned version_t;
+typedef unsigned difficulty_t;
+typedef char* buffer_t;
 
 typedef struct {
+  hash_t current_hash;
+  version_t version;
+  difficulty_t difficulty_target;
+  hash_t previous_hash;
   nonce_t nonce;
-  size_t content_size;
-} block_metadata;
+} block_metadata_t;
+
+typedef struct {
+  size_t byte_count;
+  buffer_t data;
+} content_t;
+
+typedef struct {
+  block_metadata_t metadata;
+  content_t content;
+} block_t;
+
+// read to disk
+void read(block_t*, FILE*);
+// write to disk
+void write(block_t*, FILE*);
