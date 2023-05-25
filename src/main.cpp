231191ca093aa899a96cc0369298748138787cc9
aljozu <alex.loja@utec.edu.pe>
2023-05-25 13:19:06 -0500

    Revert "libhttp"
    This reverts commit c00b90a838bee8ae0505133d85e6867802a53af4.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
-#include <httpserver.hpp>
-using namespace httpserver;
-class hello_world_resource : public http_resource {
-public:
-    std::shared_ptr<http_response> render(const http_request&) {
-      return std::shared_ptr<http_response>(new string_response("Hello, World!"));
-    }
-};
 
-int main(int argc, char** argv) {
-  webserver ws = create_webserver(8080);
-
-  hello_world_resource hwr;
-  ws.register_resource("/hello", &hwr);
-  ws.start(true);
-  
+int main(int argc, char *argv[]) {
+  std::cout << "Hello world\n";
   return 0;
 }
