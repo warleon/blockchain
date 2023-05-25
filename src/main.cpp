b851cd6a7a16ef50ddcc9aa036bc2bdc00f40e30
aljozu <alex.loja@utec.edu.pe>
2023-05-25 13:42:38 -0500

    example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <httpserver.hpp>
 
-int main(int argc, char *argv[]) {
-  std::cout << "Hello world\n";
+using namespace httpserver;
+
+class hello_world_resource : public http_resource {
+public:
+  std::shared_ptr<http_response> render(const http_request&) {
+      return std::shared_ptr<http_response>(new string_response("Hello, World!"));
+  }
+};
+
+int main(int argc, char** argv) {
+  webserver ws = create_webserver(8080);
+
+  hello_world_resource hwr;
+  ws.register_resource("/hello", &hwr);
+  ws.start(true);
+  
   return 0;
 }
