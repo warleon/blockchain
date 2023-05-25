c00b90a838bee8ae0505133d85e6867802a53af4
aljozu <alex.loja@utec.edu.pe>
2023-05-25 13:10:46 -0500

    libhttp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <httpserver.hpp>
+using namespace httpserver;
+class hello_world_resource : public http_resource {
+public:
+    std::shared_ptr<http_response> render(const http_request&) {
+      return std::shared_ptr<http_response>(new string_response("Hello, World!"));
+    }
+};
 
-int main(int argc, char *argv[]) {
-  std::cout << "Hello world\n";
+int main(int argc, char** argv) {
+  webserver ws = create_webserver(8080);
+
+  hello_world_resource hwr;
+  ws.register_resource("/hello", &hwr);
+  ws.start(true);
+  
   return 0;
 }
