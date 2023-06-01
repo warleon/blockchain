c765f0d9e9ba77571452785e4d3814eecf95e80d
warleon <51892390+warleon@users.noreply.github.com>
2023-06-01 12:46:05 -0500

    Update transaction.hpp

--- src/transaction.hpp
+++ src/transaction.hpp
@@ -1,44 +1,19 @@
 #pragma once
+#include <openssl/pem.h>
 #include <openssl/rsa.h>
 #include <openssl/sha.h>
+
+#include <fstream>
 
 typedef unsigned char hash_t[SHA256_DIGEST_LENGTH];
 typedef hash_t sign_t;
 typedef RSA* public_key_t;
 
-// typedef struct {
-//   hash_t transaction_hash;        // a hash that identifies this transaction
-//   sign_t key_hash_signature;      // a signature of the hash and public key
-//   made
-//                                   // with the private key of the owner
-//   public_key_t owner_public_key;  // the public key of the owner
-//   hash_t data_hash;  // a hash that identifies the data to be claimed
-//   // the claimed data will not be stored in the blockchain itself
-//   // but store elsewhere
-// } ownership_claim;
-// // size_t dummyvaltochecksizeinthelinter = sizeof(ownership_claim);
-// typedef struct {
-//   hash_t transaction_hash;    // a hash that identifies this transaction
-//   sign_t key_hash_signature;  // a singnature from the previous owner that
-//                               // certifies the transferece of ownership
-//   public_key_t current_owner_public_key;
-//   hash_t previous_transaction_hash;  // the hash that identifies the
-//   transaction
-//                                      // on wich this transaction is based
-// } ownership_transfer;
-// // size_t dummyvaltochecksizeinthelinter = sizeof(ownership_transfer);
-
-// typedef union {
-//   ownership_claim claim;
-//   ownership_transfer transfer;
-// } transaction_union;
-// // size_t dummyvaltochecksizeinthelinter = sizeof(transaction_union);
-
 typedef struct {
   hash_t self_hash;
   sign_t kh_signature;
-  public_key_t owner;
   hash_t hash;
+  public_key_t owner;
 } transaction_structure;
 enum transaction_enum { null, claim, transference };
 // size_t dummyvaltochecksizeinthelinter = sizeof(transaction_enum);
@@ -48,3 +23,22 @@ typedef struct {
 } transaction_t;
 // size_t dummyvaltochecksizeinthelinter = sizeof(transaction_t);
 
+void write(std::ostream& os, const transaction_t* tran) {
+  // check if the whole transaction can be written
+  BIO* bio = BIO_new(BIO_s_mem());
+  if (!bio) throw std::runtime_error("can't open bio buffer");
+
+  if (!PEM_write_bio_RSAPublicKey(bio, tran->transaction.owner)) {
+    BIO_free(bio);
+    throw std::runtime_error("can't write public key into buffer");
+  }
+
+  // write everything but the public key
+  os.write((char*)tran, sizeof(transaction_t) - sizeof(public_key_t));
+
+  // write public key
+  char* buffer;
+  long length = BIO_get_mem_data(bio, &buffer);
+  os.write(buffer, length);
+  BIO_free(bio);
+}
