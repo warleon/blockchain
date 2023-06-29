3f5e7498208d51bba04c1c81cd870c515988b164
warleon <51892390+warleon@users.noreply.github.com>
2023-06-28 19:49:42 -0500

    Update transaction.hpp

--- src/wallet/transaction.hpp
+++ src/wallet/transaction.hpp
@@ -2,10 +2,21 @@
 
 #include <string>
 
+#include "./errorCode.hpp"
 #include "./hash.hpp"
 
 namespace Transaction {
 
-Hash::type fileHash;
-std::string ownerKey;
-}
+enum category { null, claim, transference };
+typedef struct {
+  category type;
+  Hash::type hash;
+  std::string publickey;
+  std::string signature;
+  Hash::type id;
+} type;
+
+errorCode sign(type& trans, std::string privatekey) {}
+errorCode setId(type&) {}
+
+}  // namespace Transaction
